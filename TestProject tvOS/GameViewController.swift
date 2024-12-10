f42ed2a1834145007da5ba4de02c67876e39d274
DeveloperUchida <kouei28poiou@icloud.com>
2024-12-10 15:02:22 +0900

    DataUpdate

--- TestProject tvOS/GameViewController.swift
+++ TestProject tvOS/GameViewController.swift
@@ -1,29 +1,18 @@
-//
-//  GameViewController.swift
-//  TestProject tvOS
-//
-//  Created by DeveloperUchida on 2024/12/09.
-//
-
 import UIKit
 import SpriteKit
-import GameplayKit
 
-class GameViewController: UIViewController {
-    
+class GameViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
-        
-        // Present the scene
-        let skView = self.view as! SKView
-        skView.presentScene(scene)
-        
-        skView.ignoresSiblingOrder = true
-        
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        if let view = self.view as? SKView{
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .resizeFill
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
-
 }
